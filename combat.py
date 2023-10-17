66283e78d2e99f9f0462c74a2efa6684cd9a5c86
oshe8650 <oshe8650@student.uu.se>
2023-10-18 00:49:41 +0200

    player2 tillagd

--- combat.py
+++ combat.py
@@ -4,53 +4,81 @@ pg.init()
 screen = pg.display.set_mode((1440, 720))
 clock = pg.time.Clock()
 running = True
-player_pos = pg.Vector2(screen.get_width() -25, screen.get_height() / 2)
-bullet_pos = pg.Vector2(player_pos.x, player_pos.y)
+player1_pos = pg.Vector2(screen.get_width() -25, screen.get_height() / 2)
+player2_pos = pg.Vector2(25, screen.get_height() / 2)
+bullet1_pos = pg.Vector2(player1_pos.x, player1_pos.y)
 
 dt = 0
 dt1 = 0
+dt2 = 0
 cooldown = 500
 
 ##Klasser
 class Player(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, number):
         super().__init__()
-        self.image = pg.Surface((100, 100))
-        self.image.fill("white")
-        self.rect = self.image.get_rect(center = (screen.get_width() - 25, screen.get_height()/2))
+        self.number = number
+        if number == 1:
+            self.image = pg.Surface((100, 100))
+            self.image.fill("white")
+            self.rect = self.image.get_rect(center = player1_pos)
+        elif number == 2:
+            self.image = pg.Surface((100, 100))
+            self.image.fill("white")
+            self.rect = self.image.get_rect(center = player2_pos)
 
     def update(self):
         global dt1
+        global dt2
         dt1 += clock.get_time()
+        dt2 += clock.get_time()
 
-        key = pg.key.get_pressed()  
-        if key[pg.K_UP]: 
-            if self.rect.y > 0:
-                self.rect.y -= 5
-        if key[pg.K_DOWN]:
-            if self.rect.y < 720 - 100:
-                self.rect.y += 5
-        if key[pg.K_SPACE] and dt1 > cooldown:
-            dt1 = 0
-            bullet_group.add(player.shoot())
+        key = pg.key.get_pressed()
+        if self.number == 1:  
+            if key[pg.K_UP]: 
+                if self.rect.y > 0:
+                    self.rect.y -= 5
+            if key[pg.K_DOWN]:
+                if self.rect.y < 720 - 100:
+                    self.rect.y += 5
+            if key[pg.K_SPACE] and dt1 > cooldown:
+                dt1 = 0
+                bullet_group.add(player1.shoot(1))
+        elif self.number == 2:  
+            if key[pg.K_w]: 
+                if self.rect.y > 0:
+                    self.rect.y -= 5
+            if key[pg.K_s]:
+                if self.rect.y < 720 - 100:
+                    self.rect.y += 5
+            if key[pg.K_LSHIFT] and dt2 > cooldown:
+                dt2 = 0
+                bullet_group.add(player2.shoot(2))
 
-    def shoot(self):
-        return Bullet(self.rect.x, self.rect.y)
+    def shoot(self, number):
+        return Bullet(self.rect.x, self.rect.y, number)
 
 class Bullet(pg.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, number):
         super().__init__()
         self.image = pg.Surface((50,10))
         self.image.fill("red")
-        self.rect = self.image.get_rect(center = (x, y+50))
+        self.number = number
+        if number == 1:
+            self.rect = self.image.get_rect(center = (x, y+50))
+        elif number == 2:
+            self.rect = self.image.get_rect(center = (x+50, y+50))
 
     def update(self):
-        self.rect.x -= 10
-        
+        if self.number == 1:
+            self.rect.x -= 700 * dt
+        elif self.number == 2:
+            self.rect.x += 700 * dt
     
-player = Player()
+player1 = Player(1)
+player2 = Player(2)
 player_group = pg.sprite.Group()
-player_group.add(player)
+player_group.add(player1, player2)
 bullet_group = pg.sprite.Group()
 
     
